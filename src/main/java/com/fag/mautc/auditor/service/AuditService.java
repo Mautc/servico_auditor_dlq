6332b16a53bac6711c746677fbe636a8243b3ec4
Mautc <mauriliotcoloda2006@gmail.com>
2026-05-26 20:55:52 -0300

    fix: preencher QUEUE_NAME com o origin do evento em vez do nome da fila DLQ

--- src/main/java/com/fag/mautc/auditor/service/AuditService.java
+++ src/main/java/com/fag/mautc/auditor/service/AuditService.java
@@ -6,7 +6,6 @@ import com.fag.mautc.auditor.model.Severity;
 import com.fag.mautc.auditor.repository.AuditErrorRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,9 +15,6 @@ public class AuditService {
     private static final Logger log = LoggerFactory.getLogger(AuditService.class);
 
     private final AuditErrorRepository repository;
-
-    @Value("${queue.dlq-name}")
-    private String dlqName;
 
     public AuditService(AuditErrorRepository repository) {
         this.repository = repository;
@@ -30,7 +26,7 @@ public class AuditService {
 
         log.info("Triagem de severidade: total de itens={}, severity={}", evento.totalItems(), severity);
 
-        AuditError auditError = new AuditError(dlqName, payloadBruto, severity);
+        AuditError auditError = new AuditError(evento.getOrigin(), payloadBruto, severity);
         AuditError salvo = repository.save(auditError);
 
         log.info("Erro registrado com id={}, severity={}", salvo.getErrorId(), salvo.getSeverity());
